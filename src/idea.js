ef28c357e388de06c3b4de0409c600733412e753
Nathan Darrington <npdarrington@icloud.com>
2020-07-26 14:56:50 -0600

    Added deleteFromStorage and updateIdea methods

--- src/idea.js
+++ src/idea.js
@@ -8,11 +8,15 @@ class Idea {
   }
 
   saveToStorage(ideaCard) {
-    // get the poster that was just created
-    // turn that poster into a JSON string
-    // store that poster into the localstorage
     var stringifyObject = JSON.stringify(ideaCard);
     localStorage.setItem(ideaCard.id.toString(), stringifyObject);
-    console.log(localStorage);
+  }
+
+  deleteFromStorage(ideaCard) {
+
+  }
+
+  updateIdea(ideaCard) {
+    
   }
 }
