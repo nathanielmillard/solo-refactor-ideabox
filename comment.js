b51e9ef67de16ed0a9af43f4bd84973cd97b9409
Nathaniel Millard <nathanielmillard@gmail.com>
2020-07-28 13:54:48 -0600

    Create comment js

---
+++ comment.js
@@ -0,0 +1,12 @@
+class Comment {
+  constructor(newContent){
+    this.content = newContent
+    //this.content will be string values
+  }
+  saveToStorage(){
+
+  }
+  deleteFromStorage(){
+
+  };
+};
